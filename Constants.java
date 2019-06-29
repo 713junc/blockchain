13db0a11be76a570d3431161a8208130cdb0a440
jennachoi <juncr1719@gmail.com>
2019-06-29 00:36:21 -0700

    add constants

---
+++ Constants.java
@@ -0,0 +1,13 @@
+
+public class Constants {
+
+	private Constants() {
+
+	}
+
+	// mining, it is called DIFFICULTY in bitcoin, it is characterized by leading zeros.
+	public static final int DIFFICULTY = 5;
+	public static final double MINER_REWARD = 10;
+	// first blocking of the blockchain
+	public static final String GENESIS_PREV_HASH = "0000000000000000000000000000000000000000000000000000000000000000";
+}
