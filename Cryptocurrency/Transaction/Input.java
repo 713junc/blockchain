77a68a7e3ab2f286a3291a2ec3904b4876d656e1
jennachoi <juncr1719@gmail.com>
2019-07-10 21:31:07 -0700

    Input Transaction

--- Cryptocurrency/Transaction/Input.java
+++ Cryptocurrency/Transaction/Input.java
@@ -1,26 +1,26 @@
 
 public class Input { // Transaction-Input
 
-	private String outputID;
-	private Output UTXO;
+    private String outputID;
+    private Output UTXO;
 
-	public Input(String outputID) {
-		this.outputID = outputID;
-	}
+    public Input(String outputID) {
+        this.outputID = outputID;
+    }
 
-	public String getOutputID() {
-		return outputID;
-	}
+    public String getOutputID() {
+        return outputID;
+    }
+    
+    public Output getUTXO() {
+        return UTXO;
+    }
 
-	public Output getUTXO() {
-		return UTXO;
-	}
+    public void setOutputId(String outputID) {
+        this.outputID = outputID;
+    }
 
-	public void setOutputId(String outputID) {
-		this.outputID = outputID;
-	}
-
-	public void setUTXO(Output uTXO) {
-		UTXO = uTXO;
-	}
+    public void setUTXO(Output uTXO) {
+        UTXO = uTXO;
+    }
 }
